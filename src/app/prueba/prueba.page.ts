9d919858dd75be67d9ee2a67214c5db6747790b7
AlfredoOrtiz14 <40581292+AlfredoOrtiz14@users.noreply.github.com>
2019-12-07 01:17:31 -0600

    Uno

---
+++ src/app/prueba/prueba.page.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-prueba',
+  templateUrl: './prueba.page.html',
+  styleUrls: ['./prueba.page.scss'],
+})
+export class PruebaPage implements OnInit {
+
+  constructor(
+    private navCtrl: NavController
+  ) { }
+
+  ngOnInit() {
+  }
+
+  radioChecked()
+  {
+    this.navCtrl.navigateForward('/home');
+  }
+
+}
