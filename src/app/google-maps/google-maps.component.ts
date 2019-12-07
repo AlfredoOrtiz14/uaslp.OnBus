74055f2a8d4e3c9ad1eb43bcb0f77e0962f749ef
Eduardo <eduardonfoyo@gmail.com>
2019-12-07 10:54:47 -0600

    U

--- src/app/google-maps/google-maps.component.ts
+++ src/app/google-maps/google-maps.component.ts
@@ -34,26 +34,66 @@ export class GoogleMapsComponent implements OnInit {
   }
 
   loadmap() {
+    let random;
     setTimeout(() => {
-      this.map = new Map('map').setView([22.1340084, -100.985055], 14);
-      
-      // tslint:disable-next-line
-      tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',maxZoom: 18}).addTo(this.map);
+      random = Math.floor(Math.random() * (3 - 0 + 1) + 0);
+      if(random == 0){
+        this.map = new Map('map').setView([22.1340084, -100.985055], 14);
+        tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',maxZoom: 18}).addTo(this.map);
            
       let greenIcon = icon({
-          iconUrl: 'https://cdn0.iconfinder.com/data/icons/mono2/100/pin-512.png',
+          iconUrl: '../../assets/img/pin-red.png',
           iconSize: [38, 38],
           popupAnchor: [-3, -76]
       });
 
+      marker([22.1377769, -100.9891295], {icon: greenIcon}).addTo(this.map);
+      marker([22.1425126, -101.0030618], {icon: greenIcon}).addTo(this.map);
+      marker([22.133311, -100.974385], {icon: greenIcon}).addTo(this.map);
+
+      }else if(random == 1){
+        this.map = new Map('map').setView([22.1340084, -100.985055], 14);
+        tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',maxZoom: 18}).addTo(this.map);
+           
+      let greenIcon = icon({
+          iconUrl: '../../assets/img/pin-green.png',
+          iconSize: [38, 38],
+          popupAnchor: [-3, -76]
+      });
+
+      marker([22.139050, -100.997100], {icon: greenIcon}).addTo(this.map);
       marker([22.1340084, -100.985055], {icon: greenIcon}).addTo(this.map);
+      marker([22.130280, -100.976618], {icon: greenIcon}).addTo(this.map);
 
-      /*marker([22.1340084, -100.985055]).addTo(this.map)
-      .bindPopup('<img src="https://cdn0.iconfinder.com/data/icons/mono2/100/pin-512.png">')
-      .openPopup();*/
+      }else if(random == 2){
+        this.map = new Map('map').setView([22.1340084, -100.985055], 14);
+        tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',maxZoom: 18}).addTo(this.map);
+           
+      let greenIcon = icon({
+          iconUrl: '../../assets/img/pin-pink.png',
+          iconSize: [38, 38],
+          popupAnchor: [-3, -76]
+      });
 
-      //marker([22.1647257, -101.0387027]).addTo(this.map).bindPopup('A pretty CSS3 popup.<br> Easily customizable.').openPopup();
-      
+      marker([22.130956, -100.985884], {icon: greenIcon}).addTo(this.map);
+      marker([22.1387111,-100.9929029], {icon: greenIcon}).addTo(this.map);
+      marker([22.149665, -100.999094], {icon: greenIcon}).addTo(this.map);
+
+      }else if(random == 3){
+        this.map = new Map('map').setView([22.1340084, -100.985055], 14);
+        tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',maxZoom: 18}).addTo(this.map);
+           
+      let greenIcon = icon({
+          iconUrl: '../../assets/img/pin-black.png',
+          iconSize: [38, 38],
+          popupAnchor: [-3, -76]
+      });
+
+      marker([22.138200, -100.994177], {icon: greenIcon}).addTo(this.map);
+      marker([22.144310, -100.992869], {icon: greenIcon}).addTo(this.map);
+      marker([22.146846, -100.991248], {icon: greenIcon}).addTo(this.map);
+
+      }
     }, 50);
   }
 
